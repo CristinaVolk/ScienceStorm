f7919a48b352254fc379176a08596326506dbbe4
Kristina <kristinavolk4@tut.by>
2019-11-06 18:49:33 +0000

    Style changes
    Background image is fixed [edited]
    MyProjects page is properly displayed [solved]

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ScienceStorm
+
+This is Web-based application of scientific projects. The app should be cloned or downloaded. After open Visual Studio Code editor and open the downloaded project. 
+
+Please Follow the steps:
+
+1. Open your Command Line Prompt
+2. Type inside the cmd window : npm install http-server -g (to start the local       server)
+3. Hit Enter
+4. Close the Command Line Prompt
+5. Come back to Visual Studio Code and make sure you are inside the actual folder    of the downloaded application 
+6. Open the terminal and type : http-server
+7. Open your browser and go to the 'http://localhost:8080/dashboard.html'
--- js/index.js
+++ js/index.js
@@ -1,26 +1,27 @@
+//var firebase = require('firebase');
+
 var firebaseConfig = {
-  apiKey: "AIzaSyCV6EHTKmyEPPslmrsJxhzS1FV8B2OMEWo",
-  authDomain: "fir-webapp-1930b.firebaseapp.com",
-  databaseURL: "https://fir-webapp-1930b.firebaseio.com",
-  projectId: "fir-webapp-1930b",
-  storageBucket: "fir-webapp-1930b.appspot.com",
-  messagingSenderId: "934063532932",
-  appId: "1:934063532932:web:91ffa9f0ae5b5a6de3cbd1",
-  measurementId: "G-359R3H39B3"
+    apiKey: "AIzaSyCV6EHTKmyEPPslmrsJxhzS1FV8B2OMEWo",
+    authDomain: "fir-webapp-1930b.firebaseapp.com",
+    databaseURL: "https://fir-webapp-1930b.firebaseio.com",
+    projectId: "fir-webapp-1930b",
+    storageBucket: "fir-webapp-1930b.appspot.com",
+    messagingSenderId: "934063532932",
+    appId: "1:934063532932:web:91ffa9f0ae5b5a6de3cbd1",
+    measurementId: "G-359R3H39B3"
 };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  //firebase.analytics();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth.Auth.Persistence.LOCAL;
 
 
-  firebase.auth.Auth.Persistence.LOCAL;
-
-  $("#btn-login").click(function(){
+$("#btn-login").click(function() {
 
     var email = $("#email").val();
     var password = $("#password").val();
 
-    if(email !== "" && password !== ""){
+    if (email !== "" && password !== "") {
         firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
@@ -33,65 +34,75 @@ var firebaseConfig = {
     } else {
         window.alert("Form is incomplete. Please fill out all fields!");
     }
+});
 
-  });
+// For some reason the function is not working
+/*$("#btn-logout").click(function(){
+       window.alert("Passwords are not egual");
+      firebase.auth().signOut().then(function() {
+        console.log("Sign-out successful!")
+      }).catch(function(error) {
+        console.log("An error happened." + error.message); 
+      });
+});*/
 
-  $("#btn-register").click(function(){
+$("#btn-register").click(function() {
 
     var email = $("#email").val();
     var password = $("#password").val();
     var confirmPassword = $("#confirmPassword").val();
 
-    if(email !== "" && password !== "" && confirmPassword !==""){
-      if(password === confirmPassword){
-        var result = firebase.auth().createUserWithEmailAndPassword(email, password);
+    if (email !== "" && password !== "" && confirmPassword !== "") {
+        if (password === confirmPassword) {
+            var result = firebase.auth().createUserWithEmailAndPassword(email, password);
             // Handle Errors here.
             result.catch(function(error) {
 
-              var errorCode = error.code;
-              var errorMessage = error.message;
+                var errorCode = error.code;
+                var errorMessage = error.message;
 
-              console.log(errorCode);
-              console.log(errorMessage);
+                console.log(errorCode);
+                console.log(errorMessage);
 
-              window.alert("Message : " + errorMessage);
+                window.alert("Message : " + errorMessage);
             });
-        }
-        else{
-          window.alert("Passwords are not egual");
+        } else {
+            window.alert("Passwords are not egual");
         }
     } else {
         window.alert("Form is not complete. Please fill out all fields!");
     }
 
-  });
+});
 
-  $("#btn-resetPassword").click(function(){
+$("#btn-resetPassword").click(function() {
     var auth = firebase.auth();
     var email = $("#email").val();
     console.log(email);
 
-    if(email != ""){
-      auth.sendPasswordResetEmail(email).then(function(){
-        window.alert("We have sent you information with to recover your account. Please, check your emails");
-      })
-      .catch(function(error){
+    if (email != "") {
+        auth.sendPasswordResetEmail(email).then(function() {
+                window.alert("We have sent you information with to recover your account. Please, check your emails");
+            })
+            .catch(function(error) {
 
-        var errorCode = error.code;
-        var errorMessage = error.message;
+                var errorCode = error.code;
+                var errorMessage = error.message;
 
-        console.log(errorCode);
-        console.log(errorMessage);
+                console.log(errorCode);
+                console.log(errorMessage);
 
-        window.alert("Message : " + errorMessage);
-      })
-    } else{
-      window.alert("Sorry, it seems you haven't provided your email");
+                window.alert("Message : " + errorMessage);
+            })
+    } else {
+        window.alert("Sorry, it seems you haven't provided your email");
     }
-  });
+});
 
 
-  $("#btn-update").click(function(){
+
+
+$("#btn-update").click(function() {
 
     var firstname = $("#firstname").val();
     var surname = $("#surname").val();
@@ -100,41 +111,46 @@ var firebaseConfig = {
     var gender = $("#gender").val();
     var address = $("#address").val();
 
-    var rootRef = firebase.database().ref().child("Users"); 
+    var rootRef = firebase.database().ref().child("Users");
     var userID = firebase.auth().currentUser.uid;
     var usersRef = rootRef.child(userID);
-    
-    if(firstname!="" && surname!="" && country!="" && phone!="" && gender!="" && address!=""){
 
-      var userData = {
-        "firstname": firstname,
-        "surname": surname,
-        "country": country,
-        "phone": phone,
-        "gender": gender,
-        "address": address
-      };
+    if (firstname != "" && surname != "" && country != "" && phone != "" && gender != "" && address != "") {
 
-      usersRef.set(userData, function(error){
-        if(error){
+        var userData = {
+            "firstname": firstname,
+            "surname": surname,
+            "country": country,
+            "phone": phone,
+            "gender": gender,
+            "address": address
+        };
 
-          var errorCode = error.code;
-          var errorMessage = error.message;
-  
-          console.log(errorCode);
-          console.log(errorMessage);
-  
-          window.alert("Message : " + errorMessage);
+        usersRef.set(userData, function(error) {
+            if (error) {
 
-        } else {
-            window.location.href = "dashboard.html";
-        }
-      });
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                console.log(errorCode);
+                console.log(errorMessage);
+
+                window.alert("Message : " + errorMessage);
+
+            } else {
+                window.location.href = "dashboard.html";
+            }
+        });
     } else {
-      window.alert("Form is not complete. Please fill out the fields");
+        window.alert("Form is not complete. Please fill out the fields");
     }
-  });
+});
 
-  $("#dropdownMenuItem").click(function(){
-    firebase.auth().signOut();
-  });
+function switchView(view) {
+    $.get({
+        url: view,
+        cache: false,
+    }).then(function(data) {
+        $("#container").html(data);
+    });
+}
